45baa9f9b5c5224ecc78c1eb5c306bd4b3b7572a
SooBin Lim <50435598+dnpcs12@users.noreply.github.com>
2021-07-24 02:42:58 +0900

    playerState

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,12 +6,45 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
     public CollectionSite CollectionSite { get; private set; }
+
+    private int _day = 1;
+    public int Day
+    {
+        get => _day;
+        set
+        {
+            _day = value;
+        }
+    }
+    private float OneDay = 600.0f;
+
+    public float CurTime
+    {
+        get => _curTime;
+        set
+        {
+            _curTime = value;
+            // 10분이 지나면 하루.
+            if (_curTime > OneDay)
+            {
+                Day++;
+                _curTime = 0;
+            }
+            
+        }
+    }
+    private float _curTime = 0;
+    public bool isPlaying = true;
+
+
     private void Awake()
     {
         Instance = this;
     }
     private void Update()
     {
-        // 플레이어가 밝고 있는 땅을 확인하여 CollectionSite를 변경함
+        if(isPlaying) CurTime += Time.deltaTime;
+        // 플레이어가 밝고 있는 땅을 확인하여 CollectionSite를 변경함.
     }
+    
 }
