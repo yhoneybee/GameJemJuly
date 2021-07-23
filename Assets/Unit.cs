d4378a6da706cce0d3633fa826adc81293369511
kkulbeol <chaeyh04@gmail.com>
2021-07-24 03:25:27 +0900

    A* add

---
+++ Assets/Unit.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    Vector3 target;
+    public float speed = 20;
+    Vector3[] path;
+    int targetIndex;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        PathRequestManager.ReqeustPath(transform.position, target, OnPathFound);
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        if (path.Length == 0) yield break;
+        Vector3 currentWayPoint = path[0];
+
+        while (true)
+        {
+            if (transform.position == currentWayPoint)
+            {
+                ++targetIndex;
+                if (targetIndex >= path.Length)
+                    yield break;
+                currentWayPoint = path[targetIndex];
+            }
+            transform.position = Vector3.MoveTowards(transform.position, currentWayPoint, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
