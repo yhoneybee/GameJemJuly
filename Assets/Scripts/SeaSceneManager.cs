d8e56bcf56b927b0f9ce23e3ef655cc365ce93f4
BlackPenguin-cpu <82649825+BlackPenguin-cpu@users.noreply.github.com>
2021-07-25 07:56:31 +0900

    ocean

--- Assets/Scripts/SeaSceneManager.cs
+++ Assets/Scripts/SeaSceneManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SeaSceneManager : MonoBehaviour
 {
     public Player player;
     public TMPro.TMP_Text failedText;
     public GameObject btnRetry;
+    public Slider BR;
+    public Slider Limit;
     // Start is called before the first frame update
     void Start()
     {
+        Limit.value = 60;
+        BR.value = 15;
         btnRetry.SetActive(false);
         failedText.text = "사냥에 실패 했습니다.";
         failedText.gameObject.SetActive(false);
@@ -23,10 +28,17 @@ public class SeaSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player.Hp <= 0 && btnRetry.activeSelf == false)
+        if((Limit.value <= 0 || BR.value <= 0 || player.Hp <= 0) && btnRetry.activeSelf == false)
         {
             btnRetry.SetActive(true);
             failedText.gameObject.SetActive(true);
         }
+        if(player.transform.position.y>= 3)
+        {
+            BR.value += 7 * Time.deltaTime;
+        } 
+        else
+        BR.value -= Time.deltaTime;
+        Limit.value -= Time.deltaTime;
     }
 }
